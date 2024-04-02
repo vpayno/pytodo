de5d895d2ac63846be003fb866b8a9a78d53c113
Victor Payno <vpayno@users.noreply.github.com>
2024-04-02 07:51:20 -0700

    feat(pytodo): use sqlalchemy instead of sqlite3

--- manual-tests.sh
+++ manual-tests.sh
@@ -2,11 +2,67 @@
 
 set -e
 
-rm -fv todos.db
-pdm run main show
-pdm run main add running Sports
-pdm run main add sleeping Rest
-pdm run main update 1 --task reading --category Study
-pdm run main complete 1
-pdm run main delete 1
-pdm run main show
+run_cmd() {
+	printf "\n"
+	echo Running: "$@"
+	printf "\n"
+	time "$@"
+	printf "\n"
+} # run_cmd()
+
+run_sqlite3_cmd() {
+	printf "\n"
+	echo "Running: sqlite3 todos.db <- $*"
+	printf "\n"
+	run_cmd sqlite3 todos.db <<-EOF
+		${@}
+	EOF
+} # run_sqlite3_cmd()
+
+run_cmd rm -fv todos.db
+
+run_cmd pdm run main show
+
+run_sqlite3_cmd ".schema todos"
+run_sqlite3_cmd "select * from todos"
+
+for i in {1..10}; do
+	run_cmd pdm run main add "task${i}" "cat${i}"
+done
+
+run_sqlite3_cmd "select * from todos"
+
+run_cmd pdm run main update 1 --task task-one --category cat-one
+run_cmd pdm run main update 5 --task task-five --category cat-five
+
+run_cmd pdm run main complete 7
+
+run_cmd pdm run main delete 1
+
+run_cmd pdm run main complete 3
+
+run_cmd pdm run main delete 7
+
+run_cmd pdm run main show
+
+run_sqlite3_cmd "select * from todos"
+
+for i in 1{1..9}; do
+	run_cmd pdm run main add "task${i}" "cat${i}"
+done
+
+run_sqlite3_cmd "select * from todos"
+
+run_cmd pdm run main update 14 --task task-apple --category cat-apple
+run_cmd pdm run main update 17 --task task-orange --category cat-orange
+
+run_cmd pdm run main complete 15
+
+run_cmd pdm run main delete 14
+
+run_cmd pdm run main show
+
+run_sqlite3_cmd "select * from todos"
+
+printf "Manual tests completed successfully!\n"
+printf "\n"
--- src/pytodo/database.py
+++ src/pytodo/database.py
@@ -2,107 +2,178 @@
 Database
 """
 
-import sqlite3
 from datetime import datetime
+from typing import Any, Never, Tuple
+
+import sqlalchemy as db
 
 from pytodo.model import Todo
 
-conn = sqlite3.connect("todos.db")
-cursor = conn.cursor()
+engine: db.Engine = db.create_engine("sqlite:///todos.db")
+metadata: db.MetaData = db.MetaData()
 
 
 __all__ = ["insert_todo", "get_all_todos", "delete_todo", "complete_todo", "update_todo"]
 
 
-def create_table() -> None:
-    cursor.execute("""CREATE TABLE IF NOT EXISTS todos (
-        task text,
-        category text,
-        date_added text,
-        date_completed text,
-        status integer,
-        position integer
-    )""")
+def create_table(engine: db.Engine, metadata: db.MetaData) -> db.Table | None:
+    with engine.connect() as conn:
+        if metadata.tables.get("todos", None) is not None:
+            return metadata.tables["todos"]
+
+        table: db.Table = db.Table(
+            "todos",
+            metadata,
+            db.Column("task", db.String(255), nullable=False),
+            db.Column("category", db.String(255), nullable=False),
+            db.Column("date_added", db.String(255), nullable=False),
+            db.Column("date_completed", db.String(255), nullable=True),
+            db.Column("status", db.Integer(), nullable=False),
+            db.Column("position", db.Integer(), nullable=False),
+        )
+        metadata.create_all(engine)
+
+        conn.commit()
+
+        return table
+
+    return None
 
 
-create_table()
+todos: db.Table | None = create_table(engine, metadata)
 
 
 def insert_todo(todo: Todo) -> None:
-    cursor.execute("SELECT COUNT(*) FROM todos")
-    count = cursor.fetchone()[0]
+    with engine.connect() as conn:
+        if not engine.dialect.has_table(conn, "todos"):
+            # TODO: log error
+            return
 
-    # count will be off by one when count > 0
-    todo.position = max(count, 0)
+        table: db.Table = metadata.tables["todos"]
 
-    with conn:
-        cursor.execute(
-            "INSERT INTO todos VALUES(:task, :category, :date_added, :date_completed, :status, :position)",
-            {
-                "task": todo.task,
-                "category": todo.category,
-                "date_added": todo.date_added,
-                "date_completed": todo.date_completed,
-                "status": todo.status,
-                "position": todo.position,
-            },
+        stmt_count: db.Select[Tuple[int]] | db.Select[Tuple[Never]] = db.select(
+            db.func.count(table.c.position)
+        ).select_from(table)
+
+        rows: db.CursorResult[Tuple[str, int]] | db.CursorResult[Tuple[Never]] = conn.execute(stmt_count)
+
+        data: db.Row[Tuple[str, int]] | db.Row[Tuple[Never]] | None = rows.first()
+
+        # count is off by one, no need to increment
+        todo.position = data[0] if data else 0
+
+        stmt_new: db.Insert = db.insert(metadata.tables["todos"]).values(
+            task=todo.task,
+            category=todo.category,
+            date_added=todo.date_added,
+            date_completed=todo.date_completed,
+            status=todo.status,
+            position=todo.position,
         )
 
+        # comp_new: db.SQLCompiler = stmt_new.compile()
 
-def get_all_todos() -> list[Todo]:
-    cursor.execute("SELECT * FROM todos")
-    rs = cursor.fetchall()
+        _: db.CursorResult[Any] = conn.execute(stmt_new)
 
-    todos = [Todo(*result) for result in rs]
+        conn.commit()
 
-    return todos
+
+def get_all_todos() -> list[Todo] | None:
+    with engine.connect() as conn:
+        if not engine.dialect.has_table(conn, "todos"):
+            # TODO: log error
+            return None
+
+        table: db.Table = metadata.tables["todos"]
+
+        stmt_all: db.Select[Any] = table.select()
+
+        result: db.CursorResult[Any] = conn.execute(stmt_all)
+
+        todos: list[Todo] = [Todo(*row) for row in result.all()]
+
+        return todos
+
+    return None
 
 
 def delete_todo(position: int) -> None:
-    cursor.execute("SELECT COUNT(*) FROM todos")
-    count = cursor.fetchone()[0]
+    with engine.connect() as conn:
+        if not engine.dialect.has_table(conn, "todos"):
+            # TODO: log error
+            return None
 
-    with conn:
-        cursor.execute("DELETE FROM todos WHERE position=:position", {"position": position})
+        table: db.Table = metadata.tables["todos"]
 
-        for pos in range(position + 1, count):
-            change_position(pos, pos - 1, False)
+        stmt_del: db.Delete = db.delete(table).where(table.c.position == position)
+
+        _: db.CursorResult[Any] = conn.execute(stmt_del)
+
+        conn.commit()
+
+        stmt_count: db.Select[tuple[int]] = db.select(db.func.count()).select_from(table)
+
+        cursor_count: db.CursorResult[Any] = conn.execute(stmt_count)
+
+        result_count: db.Row[Any] | None = cursor_count.first()
+
+        count: int = result_count[0] if result_count else 0
+
+    for pos in range(position + 1, count):
+        change_position(pos, pos - 1, False)
 
 
 def change_position(old_position: int, new_position: int, commit: bool = True) -> None:
-    cursor.execute(
-        "UPDATE todos SET position=:new_position WHERE position=:old_position",
-        {"old_position": old_position, "new_position": new_position},
-    )
+    with engine.connect() as conn:
+        if not engine.dialect.has_table(conn, "todos"):
+            # TODO: log error
+            return None
 
-    if commit:
+        table: db.Table = metadata.tables["todos"]
+
+        stmt_update: db.Update = db.update(table).where(table.c.position == old_position).values(position=new_position)
+
+        _: db.CursorResult[Any] = conn.execute(stmt_update)
+
         conn.commit()
 
 
 def update_todo(position: int, task: str | None, category: str | None) -> None:
-    with conn:
+    with engine.connect() as conn:
+        if not engine.dialect.has_table(conn, "todos"):
+            # TODO: log error
+            return None
+
+        table: db.Table = metadata.tables["todos"]
+
         if task and category:
-            cursor.execute(
-                "UPDATE todos SET task=:task, category=:category WHERE position=:position",
-                {"position": position, "task": task, "category": category},
-            )
+            stmt_update = db.update(table).where(table.c.position == position).values(task=task, category=category)
+            _: db.CursorResult[Any] = conn.execute(stmt_update)
+            conn.commit()
 
         elif task:
-            cursor.execute(
-                "UPDATE todos SET task=:task, category=:category WHERE position=:position",
-                {"position}": position, "task": task},
-            )
+            stmt_update = db.update(table).where(table.c.position == position).values(task=task)
+            _ = conn.execute(stmt_update)
+            conn.commit()
 
         elif category:
-            cursor.execute(
-                "UPDATE todos SET category=:category WHERE position=:position",
-                {"position}": position, "category": category},
-            )
+            stmt_update = db.update(table).where(table.c.position == position).values(category=category)
+            _ = conn.execute(stmt_update)
+            conn.commit()
 
 
 def complete_todo(position: int) -> None:
-    with conn:
-        cursor.execute(
-            "UPDATE todos SET status=2, date_completed=:date_completed WHERE position=:position",
-            {"position": position, "date_completed": datetime.now().isoformat()},
+    with engine.connect() as conn:
+        if not engine.dialect.has_table(conn, "todos"):
+            # TODO: log error
+            return None
+
+        table: db.Table = metadata.tables["todos"]
+
+        stmt_update = (
+            db.update(table)
+            .where(table.c.position == position)
+            .values(status=2, date_completed=datetime.now().isoformat())
         )
+        _: db.CursorResult[Any] = conn.execute(stmt_update)
+        conn.commit()
